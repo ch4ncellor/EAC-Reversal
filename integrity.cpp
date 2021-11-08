eb0267777030a996f6714f235bb88b2f85d9f146
ch4ncellor <38055313+ch4ncellor@users.noreply.github.com>
2021-11-07 21:02:20 -0500

    Update integrity.cpp

--- integrity.cpp
+++ integrity.cpp
@@ -20,3 +20,40 @@ char EAC::Callbacks::CheckIfExportsAreHooked()
   EAC::Callbacks::ReportViolation(0xE7F46F3i64, 0i64, 0i64);
   return v0;
 }
+
+void __fastcall EAC::Callbacks::DetectAPCLevelIRQL(__int64 a1)
+{
+  __int64 v1; // rdx
+  __int64 v3; // rbx
+  void *v4; // rbp
+  struct _FAST_MUTEX *v5; // rcx
+  __int64 v6; // rbx
+
+  if ( !a1 )
+    return;
+  v1 = *(a1 + 536);
+  v3 = v1;
+  if ( !v1 )
+    goto LABEL_5;
+  if ( KeGetCurrentIrql() <= APC_LEVEL )
+  {
+    *(v1 + 32) = 1;
+    v3 = *(a1 + 536);
+LABEL_5:
+    if ( !v3 )
+      return;
+  }
+  if ( KeGetCurrentIrql() <= APC_LEVEL )
+  {
+    ExAcquireFastMutex((v3 + 40));
+    v4 = *v3;
+    v5 = (v3 + 40);
+    v6 = *(v3 + 8);
+    ExReleaseFastMutex(v5);
+    if ( v6 )
+    {
+      EAC::Callbacks::ReportViolation_1(a1, 0x50FB7A72i64, v4, (2 * v6 + 2), 0i64);
+      memset(v4, 0, 2i64 * (v6 + 1));
+    }
+  }
+}
